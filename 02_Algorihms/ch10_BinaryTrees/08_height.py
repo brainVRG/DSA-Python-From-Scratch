54e617c577f2d1dc9926369b0cc5e665724a6dce
brainVRG <104495232+brainVRG@users.noreply.github.com>
2026-03-05 17:12:50 +0900

    feat: implement recursive exists method for BSTNode

---
+++ 02_Algorihms/ch10_BinaryTrees/08_height.py
@@ -0,0 +1,172 @@
+class BSTNode:
+    def height(self):
+        """Recursively calculates the height of the tree."""
+        if not self.val:
+            return 0
+        left_height, right_height = 0, 0
+        if self.left:
+            left_height = self.left.height()
+        if self.right:
+            right_height = self.right.height()
+        return max(left_height, right_height) + 1
+
+    # don't touch below this line
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
+
+import random
+
+
+class User:
+    def __init__(self, id):
+        self.id = id
+        user_names = [
+            "Blake",
+            "Ricky",
+            "Shelley",
+            "Dave",
+            "George",
+            "John",
+            "James",
+            "Mitch",
+            "Williamson",
+            "Burry",
+            "Vennett",
+            "Shipley",
+            "Geller",
+            "Rickert",
+            "Carrell",
+            "Baum",
+            "Brownfield",
+            "Lippmann",
+            "Moses",
+        ]
+        self.user_name = f"{user_names[id % len(user_names)]}#{id}"
+
+    def __eq__(self, other):
+        return isinstance(other, User) and self.id == other.id
+
+    def __lt__(self, other):
+        return isinstance(other, User) and self.id < other.id
+
+    def __le__(self, other):
+        return isinstance(other, User) and self.id <= other.id
+
+    def __ge__(self, other):
+        return isinstance(other, User) and self.id >= other.id
+
+    def __gt__(self, other):
+        return isinstance(other, User) and self.id > other.id
+
+    def __repr__(self):
+        return "".join(self.user_name)
+
+
+def get_users(num):
+    random.seed(1)
+    users = []
+    ids = []
+    for i in range(num * 3):
+        ids.append(i)
+    random.shuffle(ids)
+    ids = ids[:num]
+    for id in ids:
+        user = User(id)
+        users.append(user)
+    return users
+
+run_cases = [
+    (2, 2),
+    (6, 3),
+]
+
+submit_cases = run_cases + [
+    (0, 0),
+    (1, 1),
+    (16, 7),
+]
+
+
+def test(num_users, expected_output):
+    users = get_users(num_users)
+    if not users:
+        root = BSTNode()
+    else:
+        root = BSTNode(users[0])
+        for user in users[1:]:
+            root.insert(user)
+
+    print("---------------------------------")
+    print(f"Users: {[str(user) for user in users]}")
+    print_tree(root)
+    print(f"Expecting height: {expected_output}")
+    result = root.height()
+    print(f"Actual height: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+def print_tree(bst_node):
+    lines = []
+    format_tree_string(bst_node, lines)
+    print("\n".join(lines))
+
+
+def format_tree_string(bst_node, lines, level=0):
+    if bst_node is not None:
+        format_tree_string(bst_node.right, lines, level + 1)
+        lines.append(" " * 4 * level + "> " + str(bst_node.val))
+        format_tree_string(bst_node.left, lines, level + 1)
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
