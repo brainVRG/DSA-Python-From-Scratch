4656378839d377e7f748cc9ad6417f559431e980
brainVRG <104495232+brainVRG@users.noreply.github.com>
2026-03-07 10:30:34 +0900

    feat: implement adjacent_nodes method to retrieve graph connections

---
+++ 02_Algorihms/ch14_Graphs/03_adjacent_nodes.py
@@ -0,0 +1,110 @@
+class Graph:
+    def adjacent_nodes(self, node):
+        """Returns a set of adjacent nodes for the given vertex, or None if it does not exist."""
+        if node in self.graph:
+            return self.graph[node]
+        else:
+            return None
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u in self.graph:
+            self.graph[u].add(v)
+        else:
+            self.graph[u] = {v}
+        if v in self.graph:
+            self.graph[v].add(u)
+        else:
+            self.graph[v] = {u}
+
+            
+run_cases = [
+    (
+        [
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            (4, 5),
+        ],
+        ([0, 1, 2, 3, 4, 5], [{1}, {0, 2}, {1, 3}, {2, 4}, {3, 5}, {4}]),
+    ),
+    (
+        [
+            (0, 1),
+            (0, 2),
+            (0, 3),
+            (1, 2),
+            (1, 3),
+        ],
+        ([0, 1, 2, 3], [{1, 2, 3}, {0, 2, 3}, {0, 1}, {0, 1}]),
+    ),
+]
+submit_cases = run_cases + [
+    (
+        [
+            (0, 2),
+            (2, 4),
+            (2, 1),
+            (3, 1),
+            (4, 5),
+        ],
+        ([0, 2, 5], [{2}, {0, 1, 4}, {4}]),
+    ),
+]
+
+
+def test(edges_to_add, test_nodes):
+    print("=================================")
+    graph = Graph()
+    for edge in edges_to_add:
+        graph.add_edge(edge[0], edge[1])
+        print(f"Added edge: {edge}")
+    print("---------------------------------")
+    try:
+        actual = []
+        for i, edge in enumerate(test_nodes[0]):
+            adj_nodes = graph.adjacent_nodes(edge)
+            actual.append(adj_nodes)
+            print(f"Adjacent nodes of {edge}:")
+            print(f" - Expecting: {test_nodes[1][i]}")
+            print(f" - Actual: {adj_nodes}")
+        if actual == test_nodes[1]:
+            print("Pass \n")
+            return True
+        print("Fail \n")
+        return False
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
